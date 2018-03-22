426101e2524ca5723607ced00ffa8a0dde6326a8
ArtemMelkumov <panderiumstudio@gmail.com>
2018-03-23 02:02:29 +0300

    SN added fonts added base colors

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {Font} from 'expo';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+
+
+    componentDidMount() {
+        Font.loadAsync({
+            'openSans-bold': require('./src/assets/fonts/OpenSans-Bold.ttf'),
+            'openSans': require('./src/assets/fonts/OpenSans-Regular.ttf'),
+            'openSans-light': require('./src/assets/fonts/OpenSans-Light.ttf'),
+        });
+    }
+
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={{ fontFamily: 'openSans-bold'}}>Open up App.js to start working on your app!</Text>
+                <Text>Changes you make will automatically reload.</Text>
+                <Text>Shake your phone to open the developer menu.</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ src/styles/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+    transparent: 'transparent',
+    white: '#fff',
+    grey: '#e0e2e7',
+    dark: '#1a1c2a',
+    accent: '#fe2042',
+    primaryText: '#0d0e15',
+    secondaryText: '#768196',
+};
